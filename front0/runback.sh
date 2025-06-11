f16f3d913f130ee7da1cf8f29ed330171b9e2372
Li, Jin <lijin1201@gmail.com>
2025-06-11 13:10:57 +0900

    Add SSL support

--- front0/runback.sh
+++ front0/runback.sh
@@ -1,2 +1,3 @@
 echo docker run -d  -p 10081:10081 --gpus all backend0
 echo docker run -d  -p 10081:10081 --gpus all --restart unless-stopped backend0
+echo docker run -d  --network=host --restart unless-stopped nginx-reverse-proxy-nginx-proxy
